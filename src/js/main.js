30dbc042601d5bf23d62ccdc3ae6e50c40c3ee4d
Kevin Twesten <4twesten@informatik.uni-hamburg.de>
2017-06-08 14:18:23 +0200

    first approach

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,9 @@ var co_time_display;
 var bq_time_display;
 var timeout;
 
+var competitorLoadTimes = [];
+var baqendLoadTimes = [];
+
 db.connect('makefast', true);
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -25,21 +28,20 @@ document.addEventListener("DOMContentLoaded", function() {
 window.reload = function() {
     co_url = $('#currentVendorUrl').val();
     if(co_url) {
-        resetComparison();
-
-        //$('#startTest').addClass('invisible');
-        $('#preWarming').removeClass('hidden');
-        $('.carousel').carousel({
-            interval: 1500,
-            wrap: false
-        });
-
         db.ready().then(() => {
             db.modules.get('checkCors', {url: co_url}).then((isCors) => {
                 if (isCors) {
-                    document.getElementById("myButton").click();
+                    $('#myButton').click();
                 } else {
-                    timeout = setTimeout(startComparison, 1);
+                    resetComparison();
+                    //$('#startTest').addClass('invisible');
+                    $('#preWarming').removeClass('hidden');
+                    $('.carousel').carousel({
+                        interval: 1500,
+                        wrap: false
+                    });
+
+                    startPreWarming();
                 }
             });
         });
@@ -50,8 +52,8 @@ window.contactUs = function(e){
     e.preventDefault();
 
     var data = {
-        name: document.getElementById('c_name').value,
-        email: document.getElementById('c_email').value,
+        name: $('#c_name').val(),
+        email: $('#c_email').val(),
         url: co_url,
         subject: 'from page speed comparison'
     };
@@ -67,29 +69,37 @@ window.contactUs = function(e){
 
 window.frameLoaded = function(iframe) {
     var stop = new Date().getTime();
+    clearInterval(timer);
     if(iframe !== 'competitor') {
-        bq_time = (stop - bq_start) / 1000;
-        clearInterval(timer);
+        if(bq_start > 0) {
+            bq_time = (stop - bq_start) / 1000;
+            baqendLoadTimes.push(stop);
+        }
     } else {
-        co_time = (stop - co_start) / 1000;
-        clearInterval(timer);
-        $('.co_time').html(co_time.toFixed(3) + 's');
-        timeout = setTimeout(function () {
-            $('.iframe-baqend').html(
-                '<iframe ' + '' +
-                'src="about:blank" id="iframe_baqend" ' +
-                'class="myframe" name="competitor" onload="frameLoaded()">' +
-                '</iframe>');
-            var frame = $('#iframe_baqend');
-            frame.addClass('loading');
-            frame.prop('src', 'https://makefast-clone.baqend.com/index.html?url=' + co_url);
-            bq_start = new Date().getTime();
-            timer = setInterval(updateTime, 1, bq_time_display, bq_start);
-        }, 1000);
+        if(co_start > 0) {
+            co_time = (stop - co_start) / 1000;
+            competitorLoadTimes.push(stop);
+            $('.co_time').html(co_time.toFixed(3) + 's');
+            timeout = setTimeout(function () {
+                $('.iframe-baqend').html(
+                    '<iframe ' +
+                    'src="about:blank" id="iframe_baqend" ' +
+                    'class="myframe" name="competitor" onload="frameLoaded()">' +
+                    '</iframe>');
+                var frame = $('#iframe_baqend');
+                frame.addClass('loading');
+                frame.prop('src', 'https://makefast-clone.baqend.com?url=' + encodeURIComponent(co_url));
+                bq_start = new Date().getTime();
+                timer = setInterval(updateTime, 1, bq_time_display, bq_start);
+            }, 1000);
+        }
     }
+
+    console.log(baqendLoadTimes);
+    console.log(competitorLoadTimes);
 };
 
-function startComparison() {
+window.startComparison = function() {
     var name = 'competitor';
     $('.iframe-competitor').html(
         '<iframe ' +
@@ -102,6 +112,17 @@ function startComparison() {
 
     co_start = new Date().getTime();
     timer = setInterval(updateTime, 1, co_time_display, co_start);
+};
+
+function startPreWarming() {
+    $('.iframe-baqend').html(
+        '<iframe ' + '' +
+        'src="about:blank" id="iframe_baqend" ' +
+        'class="myframe" name="competitor" onload="startComparison()">' +
+        '</iframe>');
+
+    var frame = $('#iframe_baqend');
+    frame.prop('src', 'https://makefast-clone.baqend.com?url=' + encodeURIComponent(co_url));
 }
 
 function resetComparison() {
