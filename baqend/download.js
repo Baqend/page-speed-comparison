424d9997f63c35677a14280d821c4860a52b170e
felix <fg@baqend.com>
2017-08-03 11:36:31 +0200

    added DL script

--- baqend/download.js
+++ baqend/download.js
@@ -1,14 +1,42 @@
 const http = require('http');
-exports.call = function(db, url, target) {
+
+/**
+ * Fetches a URL an saves it to a Baqend file.
+ *
+ * Usage example:
+ * download = require('./download');
+ * return download.toFile(db, "http://...test.jpg", "/www/image.jpg", 0);
+ *
+ * @param db Baqend db to use
+ * @param url the URL that should be downloaded
+ * @param target the name of the Baqend file to save it into
+ * @param maxRetries number of retries if the response is a 4xx or 5xx status code
+ * @returns {Promise} a Promise that resolved to the uploaded file
+ */
+exports.call = function(db, url, target, maxRetries = 10) {
     return new Promise((resolve, reject) => {
         http.get(url, (res) => {
             const file = new db.File({path: target});
             const size = res.headers['content-length'];
             const mimeType = res.headers['content-type'];
+
+            //Retry on error
+            if (res.statusCode >= 400 && res.statusCode < 600) {
+                if(maxRetries <= 0) {
+                    reject(new Error("Maximum number of retries reached without success"));
+                } else {
+                    resolve(get(db, url, target, maxRetries - 1));
+                }
+                return;
+            }
+
+            //Full response
             if (size) {
                 resolve(file.upload({mimeType, size, type: 'stream', data: res}));
                 return;
             }
+
+            //Or chunked encoding
             const chunks = [];
             res.on('data', chunks.push.bind(chunks));
             res.on('end', () => {
