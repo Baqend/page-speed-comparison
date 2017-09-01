9107ca5f519e71c293ffe2867115caecf0400432
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2017-09-01 17:21:56 +0200

    Put math functions into maths.js

--- src/js/TestResultHandler.js
+++ src/js/TestResultHandler.js
@@ -1,32 +1,9 @@
 import { UiElementCreator } from './UiElementCreator';
 import { ConvertBytesService } from './ConvertBytesService';
+import { roundToTenths, roundToHundredths } from './maths';
 
 const uiElementCreator = new UiElementCreator();
 const convertBytesService = new ConvertBytesService();
-
-/**
- * Rounds a floating point number to tenth.
- *
- * E.g.: 4.555435345345 -> 4.6
- *
- * @param {number} num The number to round.
- * @return {number} The rounded number.
- */
-export function roundToTenths(num) {
-    return Math.round(num * 10) / 10;
-}
-
-/**
- * Rounds a floating point number to hundredth.
- *
- * E.g.: 4.555435345345 -> 4.56
- *
- * @param {number} num The number to round.
- * @return {number} The rounded number.
- */
-export function roundToHundredths(num) {
-    return Math.round(num * 100) / 100;
-}
 
 export class TestResultHandler {
 
