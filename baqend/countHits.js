8fa48a93d00623f1685164de90bf98d7b2f49f16
Kevin Twesten <kt@baqend.com>
2018-01-29 17:19:41 +0100

    Count size of served requests

--- baqend/countHits.js
+++ baqend/countHits.js
@@ -5,16 +5,22 @@ const { countBy } = require('lodash');
  * @return {Object<string, number>}
  */
 function countHits(requests) {
-  return countBy(requests, (req) => {
+  let size = 0;
+  const count = countBy(requests, (req) => {
     const headers = req.headers;
     if (headers) {
       const resHeaders = headers.response.join(' ').toLowerCase();
       if (resHeaders.indexOf('via: baqend') !== -1) {
+        const contentHeader = headers.response.find(item => item.includes('content-length'));
+        size += contentHeader ? parseInt(contentHeader.substring(contentHeader.indexOf(':') + 1).trim(), 10) : 0;
         return resHeaders.indexOf('x-cache: hit') !== -1 ? 'hit' : 'miss';
       }
     }
     return 'other';
   });
+
+  count.size = size;
+  return count;
 }
 
 exports.countHits = countHits;
